5bc05a90d3bac59dbe00c4e3d97eac308949809f
Craig Oliver <coliver988@gmail.com>
2023-11-03 19:24:38 -0400

    Add project files.

---
+++ PocRestAPI/Models/JsonOrder.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+
+namespace PocRestAPI.Models
+{
+    public class JsonOrder
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class Attributes
+        {
+            [JsonProperty("vendor-po")]
+            public string VendorPo { get; set; }
+
+            [JsonProperty("shipping-method")]
+            public string ShippingMethod { get; set; }
+
+            [JsonProperty("shipping-account-number")]
+            public string ShippingAccountNumber { get; set; }
+
+            [JsonProperty("test-order")]
+            public string TestOrder { get; set; }
+
+            [JsonProperty("shipping-details")]
+            public ShippingDetails ShippingDetails { get; set; }
+        }
+
+        public class Root
+        {
+            public string Type { get; set; }
+            public Attributes Attributes { get; set; }
+        }
+
+        public class ShippingDetails
+        {
+            public string IOSS { get; set; }
+            public string TrackingUrl { get; set; }
+            public string TrackingNumber { get; set; }
+        }
+
+
+    }
+}
---
+++ PocRestAPI/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace PocRestAPI.Models
+{
+    public class Order
+    {
+        public Order() { }
+        /// <summary>
+        /// create an order from a JSON payload
+        /// </summary>
+        /// <param name="jsonOrder"></param>
+        /// <note>no logic but returning true for tests</note>
+        public Order(JsonOrder jsonOrder)
+        {
+            isValid = true;
+        }
+
+        public bool isValid;
+        public int orderId;
+    }
+}
