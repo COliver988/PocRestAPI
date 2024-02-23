73ee76383619204c2e00981736473de33907dc4f
Craig Oliver <craigo@manualww.com>
2024-02-23 14:44:40 -0500

    switched from class to record

--- PocRestAPI/Models/JsonOrder.cs
+++ PocRestAPI/Models/JsonOrder.cs
@@ -2,40 +2,38 @@
 
 namespace PocRestAPI.Models
 {
-    public class JsonOrder
+    public record JsonOrder
     {
-        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-        public class Attributes
+        // Root myDeserializedrecord = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public record Attributes
         {
             [JsonProperty("vendor-po")]
-            public string VendorPo { get; set; }
+            public required string VendorPo { get; set; }
 
             [JsonProperty("shipping-method")]
-            public string ShippingMethod { get; set; }
+            public required string ShippingMethod { get; set; }
 
             [JsonProperty("shipping-account-number")]
-            public string ShippingAccountNumber { get; set; }
+            public required string ShippingAccountNumber { get; set; }
 
             [JsonProperty("test-order")]
-            public string TestOrder { get; set; }
+            public string? TestOrder { get; set; }
 
             [JsonProperty("shipping-details")]
             public ShippingDetails ShippingDetails { get; set; }
         }
 
-        public class Root
+        public record Root
         {
-            public string Type { get; set; }
+            public string? Type { get; set; }
             public Attributes Attributes { get; set; }
         }
 
-        public class ShippingDetails
+        public record ShippingDetails
         {
-            public string IOSS { get; set; }
-            public string TrackingUrl { get; set; }
-            public string TrackingNumber { get; set; }
+            public string? IOSS { get; set; }
+            public string? TrackingUrl { get; set; }
+            public string? TrackingNumber { get; set; }
         }
-
-
     }
 }
